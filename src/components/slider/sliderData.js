94529dad8c0eb134d1eaea0aa99ffa396c963a79
VitorAVPereira <v.virginio.pereira@accenture.com>
2023-09-09 15:41:58 -0300

    images

--- src/components/slider/sliderData.js
+++ src/components/slider/sliderData.js
@@ -1,14 +1,14 @@
 export const SliderData = [
     {
         image:
-        '../../../public/image1.jpg'
+        'https://images.unsplash.com/photo-1682687982502-1529b3b33f85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
     },
     {
         image:
-        '../../../public/image2.jpg'
+        'https://images.unsplash.com/photo-1613506140303-a3742bcbee78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80'
     },
     {
         image:
-        '../../../public/image3.jpg'
+        'https://images.unsplash.com/photo-1520645521318-f03a712f0e67?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
     }
 ]
